7be4c7cc6f4cb35be59b3f9de83e2b88270c9015
joginjoy <joginjoy@gmail.com>
2020-10-13 20:58:29 +0200

    created service to call the api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { RandomJokeServiceComponent } from './services/random-joke-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RandomJokeServiceComponent
   ],
   imports: [
     BrowserModule,
